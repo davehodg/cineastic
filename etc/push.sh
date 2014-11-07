f8a7c6f54000c6265c71154b66265e14980f53be
David Hodgkinson <daveh@hodgkinson.org>
2014-11-07 17:50:17 +0000

    exclude sql dir from rsync, get ready for sqlt-diff

--- etc/push.sh
+++ etc/push.sh
@@ -10,7 +10,11 @@ export PERL5LIB=lib:$PERL5LIB
 #prove -r t
 
 
-rsync -Crvaz --delete . www.cineastic.co.uk:~/cineastic
+mysqldump -uroot -d cineastic >sql/schema_local.sql
+ssh www.cineastic.co.uk 'mysqldump -uroot -pxmysqlx -d cineastic' >sql/schema_live.sql
+
+
+rsync -Crvaz --exclude=sql/ --delete . www.cineastic.co.uk:~/cineastic
 
 #ssh www.cineastic.co.uk "(source perl5/perlbrew/etc/bashrc; cd cineastic ;  PERL5LIB=lib cpanm .)"
 
