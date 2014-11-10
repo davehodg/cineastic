e8473a7bb038257b29ec47c38a0e839f838cef4d
David Hodgkinson <daveh@hodgkinson.org>
2014-11-10 14:39:18 +0000

    add ignore .git

--- etc/push.sh
+++ etc/push.sh
@@ -14,7 +14,7 @@ mysqldump -uroot -d cineastic >sql/schema_local.sql
 ssh www.cineastic.co.uk 'mysqldump -uroot -pxmysqlx -d cineastic' >sql/schema_live.sql
 
 
-rsync -Crvaz --exclude=sql/ --delete . www.cineastic.co.uk:~/cineastic
+rsync -Crvaz --exclude=sql/ --exclude=.git/ --delete . www.cineastic.co.uk:~/cineastic
 
 #ssh www.cineastic.co.uk "(source perl5/perlbrew/etc/bashrc; cd cineastic ;  PERL5LIB=lib cpanm .)"
 
